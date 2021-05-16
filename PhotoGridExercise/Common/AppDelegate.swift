6e109832a35394ec7fe8507ce1f0a8041bf2801c
David Diego Gomez <diegodavid@icloud.com>
2021-05-16 15:37:35 -0300

    first commit

---
+++ PhotoGridExercise/Common/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  PhotoGridExercise
+//
+//  Created by David Diego Gomez on 16/05/2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ PhotoGridExercise/View/PhotoGridExcerciseViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoGridExercise
+//
+//  Created by David Diego Gomez on 16/05/2021.
+//
+
+import UIKit
+
+class PhotoGridExcerciseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
